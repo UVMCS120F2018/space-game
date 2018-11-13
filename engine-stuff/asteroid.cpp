746ef33f92fea7380dae1ab19c2db25a1aee4dd0
Zach Bernstein <zebernst@gmail.com>
2018-11-13 10:47:18 -0500

    add asteroid

---
+++ engine-stuff/asteroid.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zach Bernstein on 11/13/18.
+//
+
+#include "asteroid.h"
---
+++ engine-stuff/asteroid.h
@@ -0,0 +1,16 @@
+//
+// Created by Zach Bernstein on 11/13/18.
+//
+
+#ifndef SPACE_GAME_ASTEROID_H
+#define SPACE_GAME_ASTEROID_H
+
+
+#include "Circle.h"
+
+class Asteroid : public Circle {
+    // they dont do anything lol
+};
+
+
+#endif //SPACE_GAME_ASTEROID_H
